856b13f4acb4b50675c006399c4539c3c8d8ac03
sONg20NOW <sgk1004s@naver.com>
2025-10-13 18:04:19 +0900

    탭 구현 완료

--- src/app/(mobile)/check/[id]/inspections/[machineInspectionId]/page.tsx
+++ src/app/(mobile)/check/[id]/inspections/[machineInspectionId]/page.tsx
@@ -1,17 +1,33 @@
 'use client'
 
+import { useContext, useState } from 'react'
+
 import { useRouter } from 'next/navigation'
 
-import { Box, IconButton } from '@mui/material'
+import { Box, IconButton, Tab } from '@mui/material'
+
+import TabList from '@mui/lab/TabList'
+
+import TabContext from '@mui/lab/TabContext'
+
+import TabPanel from '@mui/lab/TabPanel'
 
 import MobileHeader from '@/app/(mobile)/_components/MobileHeader'
+import { isMobileContext } from '@/app/_components/ProtectedPage'
+
+type currentTabType = 'pictures' | 'info' | 'gallery' | 'camera'
 
 export default function CheckInspectionDetailPage() {
   const router = useRouter()
   const inspectionName = '설비명'
 
+  const isMobile = useContext(isMobileContext)
+
+  const [currentTab, setCurrentTab] = useState<currentTabType>('pictures')
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100dvh' }}>
+      {/* 헤더 */}
       <MobileHeader
         left={
           <IconButton sx={{ p: 0 }} onClick={() => router.back()}>
@@ -20,6 +36,27 @@ export default function CheckInspectionDetailPage() {
         }
         title={`${inspectionName}`}
       />
+      {/* 본 컨텐츠 (스크롤 가능 영역)*/}
+      <TabContext value={currentTab}>
+        <Box sx={{ flex: 1 }}>
+          <TabPanel value={'pictures'}>1</TabPanel>
+          <TabPanel value={'info'}>2</TabPanel>
+          <TabPanel value={'gallery'}>3</TabPanel>
+          <TabPanel value={'camera'}>4</TabPanel>
+        </Box>
+        <Box sx={{ borderTop: 1, borderColor: 'divider' }}>
+          <TabList
+            sx={{ display: 'flex', px: isMobile ? '' : 20 }}
+            centered
+            onChange={(event: React.SyntheticEvent, newValue: currentTabType) => setCurrentTab(newValue)}
+          >
+            <Tab sx={{ flex: 1 }} value={'pictures'} label={<i className='tabler-photo text-4xl' />}></Tab>
+            <Tab sx={{ flex: 1 }} value={'info'} label={<i className='tabler-info-circle text-4xl' />}></Tab>
+            <Tab sx={{ flex: 1 }} value={'gallery'} label={<i className='tabler-library-photo text-4xl' />}></Tab>
+            <Tab sx={{ flex: 1 }} value={'camera'} label={<i className='tabler-photo-sensor text-4xl' />}></Tab>
+          </TabList>
+        </Box>
+      </TabContext>
     </Box>
   )
 }
