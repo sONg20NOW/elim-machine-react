f9797449b19685ab3a0ec21b75a02adbc9f497c0
sONg20NOW <sgk1004s@naver.com>
2025-10-13 17:39:03 +0900

    inspection 디테일 페이지 기초 디자인

--- src/app/(mobile)/check/[id]/inspections/[machineInspectionId]/page.tsx
+++ src/app/(mobile)/check/[id]/inspections/[machineInspectionId]/page.tsx
@@ -1,3 +1,25 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { Box, IconButton } from '@mui/material'
+
+import MobileHeader from '@/app/(mobile)/_components/MobileHeader'
+
 export default function CheckInspectionDetailPage() {
-  return null
+  const router = useRouter()
+  const inspectionName = '설비명'
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100dvh' }}>
+      <MobileHeader
+        left={
+          <IconButton sx={{ p: 0 }} onClick={() => router.back()}>
+            <i className='tabler-chevron-left text-white text-3xl' />
+          </IconButton>
+        }
+        title={`${inspectionName}`}
+      />
+    </Box>
+  )
 }
