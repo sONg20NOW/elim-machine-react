75ab2c1837048208171f9b11457c642dbf8f3dc2
sONg20NOW <sgk1004s@naver.com>
2025-10-21 15:34:19 +0900

    정보 수정 / 삭제 구현 완료

--- src/app/(mobile)/check/[id]/inspections/[machineInspectionId]/pictures/page.tsx
+++ src/app/(mobile)/check/[id]/inspections/[machineInspectionId]/pictures/page.tsx
@@ -2,42 +2,132 @@
 
 import { useCallback, useContext, useEffect, useRef, useState } from 'react'
 
-import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useParams, useRouter, useSearchParams } from 'next/navigation'
 
-import Image from 'next/image'
+import { Box, IconButton, InputLabel, TextField, Typography } from '@mui/material'
 
-import { Box, IconButton, TextField, Typography } from '@mui/material'
+import { useForm } from 'react-hook-form'
 
 import MobileHeader from '@/app/(mobile)/_components/MobileHeader'
 import { isMobileContext } from '@/@core/components/custom/ProtectedPage'
 import DeleteModal from '@/@core/components/custom/DeleteModal'
 import { auth } from '@/lib/auth'
-import type { MachinePicCursorType, MachinePicPresignedUrlResponseDtoType } from '@/@core/types'
+import type {
+  machineChecklistItemsWithPicCountResponseDtosType,
+  MachinePicCursorType,
+  MachinePicPresignedUrlResponseDtoType,
+  MachinePicUpdateResponseDtoType
+} from '@/@core/types'
+import { handleApiError, handleSuccess } from '@/utils/errorHandler'
 
 const max_pic = 100
+
+interface formType {
+  version: number
+  machinePicId: number
+  machineInspectionId: number
+  machineChecklistSubItemId: number
+  originalFileName: string
+  alternativeSubTitle: string
+  measuredValue: string
+  remark: string
+}
 
 export default function PicturePage() {
   const { id: machineProjectId, machineInspectionId } = useParams()
 
   const router = useRouter()
   const searchParams = useSearchParams()
-  const picId = searchParams.get('picId')
+  const initialPicId = searchParams.get('picId')
 
   const isMobile = useContext(isMobileContext)
 
   const saveButtonRef = useRef<HTMLElement>(null)
   const scrollableAreaRef = useRef<HTMLElement>(null)
+  const imageInputRef = useRef<HTMLInputElement>(null)
 
   const inspectionName = 'default'
   const [pictures, setPictures] = useState<MachinePicPresignedUrlResponseDtoType[]>([])
-  const [selectedPicId, setSelectedPicId] = useState(Number(picId))
+  const [selectedPicId, setSelectedPicId] = useState(Number(initialPicId))
   const selectedPic = pictures.find(v => v.machinePicId === selectedPicId)
 
   const [openAlert, setOpenAlert] = useState(false)
 
-  async function handleDeletePicture() {
+  const [checkiistList, setCheckiistList] = useState<machineChecklistItemsWithPicCountResponseDtosType[]>([])
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { isDirty }
+  } = useForm<formType>({
+    defaultValues: {
+      machinePicId: 0,
+      version: 0,
+      machineInspectionId: 0,
+      machineChecklistSubItemId: 0,
+      originalFileName: '',
+      alternativeSubTitle: '',
+      measuredValue: '',
+      remark: ''
+    }
+  })
+
+  useEffect(() => {
+    console.log('select picture:', selectedPic)
+    if (selectedPic)
+      reset({
+        version: selectedPic.version,
+        machinePicId: selectedPic.machinePicId,
+        machineInspectionId: selectedPic.machineInspectionId,
+        machineChecklistSubItemId: selectedPic.machineChecklistSubItemId ?? 0,
+        originalFileName: selectedPic.originalFileName ?? '',
+        alternativeSubTitle: selectedPic.alternativeSubTitle ?? '',
+        measuredValue: selectedPic.measuredValue ?? '',
+        remark: selectedPic.remark
+      })
+  }, [selectedPic, reset])
+
+  const handleDeletePicture = useCallback(async () => {
+    try {
+      await auth.delete(`/api/machine-projects/${machineProjectId}/machine-pics`, {
+        // @ts-ignore
+        data: {
+          machinePicDeleteRequestDtos: [{ machinePicId: getValues().machinePicId, version: getValues().version }]
+        }
+      })
+
+      const rest = pictures.filter(v => v.machinePicId !== selectedPicId)
+
+      if (rest.length) {
+        setSelectedPicId(rest[0].machinePicId)
+        setPictures(rest)
+      } else {
+        router.back()
+      }
+
+      setOpenAlert(false)
+    } catch (e) {
+      handleApiError(e)
+    }
+
     return
-  }
+  }, [machineProjectId, selectedPicId, getValues, pictures, router])
+
+  // inspectionId가 바뀔 때마다 점검항목 가져오기
+  const getChecklistList = useCallback(async () => {
+    const response = await auth.get<{
+      data: { machineChecklistItemsWithPicCountResponseDtos: machineChecklistItemsWithPicCountResponseDtosType[] }
+    }>(`/api/machine-projects/${machineProjectId}/machine-inspections/${machineInspectionId}`)
+
+    setCheckiistList(response.data.data.machineChecklistItemsWithPicCountResponseDtos)
+    console.log('get checklist items succeed: ', response.data.data.machineChecklistItemsWithPicCountResponseDtos)
+  }, [machineProjectId, machineInspectionId])
+
+  useEffect(() => {
+    getChecklistList()
+  }, [getChecklistList])
 
   // 모든 사진 가져오기
   const getAllPictures = useCallback(async () => {
@@ -60,6 +150,29 @@ export default function PicturePage() {
   useEffect(() => {
     getAllPictures()
   }, [getAllPictures])
+
+  const handleSave = useCallback(
+    async (data: formType) => {
+      try {
+        const response = await auth
+          .put<{
+            data: MachinePicUpdateResponseDtoType
+          }>(
+            `/api/machine-projects/${machineProjectId}/machine-inspections/${machineInspectionId}/machine-pics/${selectedPicId}`,
+            data
+          )
+          .then(v => v.data.data)
+
+        setPictures(prev => prev.map(v => (v.machinePicId === selectedPicId ? { ...v, ...response } : v)))
+        reset(response)
+        console.log('updated picture:', response)
+        handleSuccess(`사진정보가 수정되었습니다.`)
+      } catch (e) {
+        handleApiError(e)
+      }
+    },
+    [machineProjectId, selectedPicId, machineInspectionId, reset]
+  )
 
   function TinyImgCard({ pic }: { pic: MachinePicPresignedUrlResponseDtoType }) {
     const imageRef = useRef<HTMLElement>(null)
@@ -96,72 +209,128 @@ export default function PicturePage() {
   }
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100dvh' }}>
-      {/* 헤더 */}
-      <MobileHeader
-        left={
-          <IconButton sx={{ p: 0 }} onClick={() => router.back()}>
-            <i className='tabler-chevron-left text-white text-3xl' />
-          </IconButton>
-        }
-        right={
-          <Box sx={{ display: 'flex', gap: isMobile ? 2 : 4 }}>
-            <IconButton sx={{ p: 0 }}>
-              <i ref={saveButtonRef} className=' tabler-device-floppy text-white text-3xl' />
+    <form onSubmit={handleSubmit(handleSave)}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100dvh' }}>
+        {/* 헤더 */}
+        <MobileHeader
+          left={
+            <IconButton type='button' sx={{ p: 0 }} onClick={() => router.back()}>
+              <i className='tabler-chevron-left text-white text-3xl' />
             </IconButton>
-            <IconButton sx={{ p: 0 }} onClick={() => setOpenAlert(true)}>
-              <i className='tabler-trash-filled text-red-400 text-3xl' />
+          }
+          right={
+            <Box sx={{ display: 'flex', gap: isMobile ? 2 : 4 }}>
+              <IconButton type='submit' sx={{ p: 0 }}>
+                <i
+                  ref={saveButtonRef}
+                  className={`tabler-device-floppy text-white text-3xl ${isDirty ? 'animate-ring' : ''}`}
+                />
+              </IconButton>
+              <IconButton type='button' sx={{ p: 0 }} onClick={() => setOpenAlert(true)}>
+                <i className='tabler-trash-filled text-red-400 text-3xl' />
+              </IconButton>
+            </Box>
+          }
+          title={inspectionName + initialPicId}
+        />
+        {/* 본 컨텐츠 (스크롤 가능 영역)*/}
+        <Box
+          ref={scrollableAreaRef}
+          sx={{
+            flex: 1,
+            overflowY: 'auto',
+            py: !isMobile ? 10 : 4,
+            px: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 5
+          }}
+        >
+          <Box sx={{ height: '40dvh', border: '1px solid lightgray', borderRadius: 2, p: 2, position: 'relative' }}>
+            <IconButton type='button' sx={{ position: 'absolute', right: 8, top: 8, color: '#3477FE' }}>
+              <i className='tabler-camera text-4xl' onClick={() => imageInputRef.current?.click()} />
             </IconButton>
+            <input type='file' hidden ref={imageInputRef} />
+
+            {selectedPic ? (
+              <img
+                src={selectedPic.presignedUrl}
+                alt={selectedPic.alternativeSubTitle}
+                style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+              />
+            ) : (
+              <Typography>이미지 오류!</Typography>
+            )}
           </Box>
-        }
-        title={inspectionName + picId}
-      />
-      {/* 본 컨텐츠 (스크롤 가능 영역)*/}
-      <Box
-        ref={scrollableAreaRef}
-        sx={{
-          flex: 1,
-          overflowY: 'auto',
-          py: !isMobile ? 10 : 4,
-          px: 10,
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 5
-        }}
-      >
-        <Box sx={{ height: '40dvh', border: '1px solid lightgray', borderRadius: 2, p: 2, position: 'relative' }}>
-          <IconButton size='large' sx={{ position: 'absolute', right: 0, top: 0 }}>
-            <i className='tabler-camera' />
-          </IconButton>
-          {selectedPic ? (
-            <img
-              src={selectedPic.presignedUrl}
-              alt={selectedPic.alternativeSubTitle}
-              style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+          {/* <TextField {...register('')} fullWidth select>
+            {checkiistList.map(v => (
+              <MenuItem key={v.machineChecklistItemId} value={v.machineChecklistItemId}>
+                {v.machineChecklistItemName}
+              </MenuItem>
+            ))}
+          </TextField> */}
+          <TextField {...register('machineChecklistSubItemId')} fullWidth />
+          <div className='flex flex-col gap-1'>
+            <InputLabel sx={{ px: 2 }}>파일 이름</InputLabel>
+            <TextField
+              size={isMobile ? 'small' : 'medium'}
+              fullWidth
+              {...register('originalFileName')}
+              hiddenLabel
+              multiline
+              slotProps={{ input: { sx: { fontSize: 18 } } }}
             />
-          ) : (
-            <Typography>이미지 오류!</Typography>
-          )}
+          </div>
+          <div className='flex flex-col gap-1'>
+            <InputLabel sx={{ px: 2 }}>대체타이틀</InputLabel>
+            <TextField
+              size={isMobile ? 'small' : 'medium'}
+              fullWidth
+              {...register('alternativeSubTitle')}
+              hiddenLabel
+              multiline
+              slotProps={{ input: { sx: { fontSize: 18 } } }}
+            />
+          </div>
+          <div className='flex flex-col gap-1'>
+            <InputLabel sx={{ px: 2 }}>측정값</InputLabel>
+            <TextField
+              size={isMobile ? 'small' : 'medium'}
+              fullWidth
+              {...register('measuredValue')}
+              hiddenLabel
+              multiline
+              slotProps={{ input: { sx: { fontSize: 18 } } }}
+            />
+          </div>
+          <div className='flex flex-col gap-1'>
+            <InputLabel sx={{ px: 2 }}>비고</InputLabel>
+            <TextField
+              size={isMobile ? 'small' : 'medium'}
+              fullWidth
+              {...register('remark')}
+              hiddenLabel
+              multiline
+              slotProps={{ input: { sx: { fontSize: 18 } } }}
+            />
+          </div>
         </Box>
-        <TextField fullWidth />
-        <TextField fullWidth />
-        <TextField fullWidth />
+        {/* 탭 리스트 */}
+        <Box
+          sx={{
+            p: 2,
+            display: 'flex',
+            gap: 2,
+            overflowX: 'scroll',
+            width: '100dvw'
+          }}
+        >
+          {pictures.map(pic => (
+            <TinyImgCard key={pic.machinePicId} pic={pic} />
+          ))}
+        </Box>
+        <DeleteModal showDeleteModal={openAlert} setShowDeleteModal={setOpenAlert} onDelete={handleDeletePicture} />
       </Box>
-      {/* 탭 리스트 */}
-      <Box
-        sx={{
-          p: 2,
-          display: 'flex',
-          gap: 2,
-          overflowX: 'scroll',
-          width: '100dvw'
-        }}
-      >
-        {pictures.map(pic => (
-          <TinyImgCard key={pic.machinePicId} pic={pic} />
-        ))}
-      </Box>
-      <DeleteModal showDeleteModal={openAlert} setShowDeleteModal={setOpenAlert} onDelete={handleDeletePicture} />
-    </Box>
+    </form>
   )
 }
