3fd1e04acbfc7e173f4db77332a6fc207a04e6ab
sONg20NOW <sgk1004s@naver.com>
2025-10-29 14:28:12 +0900

    더 빨리 로딩되도록

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import { useRouter } from 'next/navigation'
 
@@ -12,27 +12,18 @@ export default function HomeRedirectPage() {
   const router = useRouter()
   const isTablet = useIsTablet()
 
-  const [flag, setFlag] = useState(false)
-
   useEffect(() => {
-    setTimeout(() => {
-      setFlag(true)
-      setTimeout(() => {
-        if (isTablet) {
-          router.replace('/check')
-        } else {
-          router.replace('/machine')
-        }
-      }, 1000)
-    }, 1000)
+    if (isTablet) {
+      router.replace('/check')
+    } else {
+      router.replace('/machine')
+    }
   }, [router, isTablet])
 
   return (
     <Backdrop open>
       <div className='flex flex-col gap-8 items-center'>
-        <Typography color='white'>
-          {flag ? '엘림 기계설비현장 웹페이지로 이동 중...' : '사용자 정보를 불러오는 중...'}
-        </Typography>
+        <Typography color='white'>{'엘림 기계설비현장 웹페이지로 이동 중...'}</Typography>
         <CircularProgress size={60} />
       </div>
     </Backdrop>
