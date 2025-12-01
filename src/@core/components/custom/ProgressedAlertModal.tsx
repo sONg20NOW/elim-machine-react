cf253ce2f67a712a60cd5a25f5412c7b73f1f081
sONg20NOW <sgk1004s@naver.com>
2025-12-01 10:18:45 +0900

    chore: title, confirmMessage props 추가

--- src/@core/components/custom/ProgressedAlertModal.tsx
+++ src/@core/components/custom/ProgressedAlertModal.tsx
@@ -5,27 +5,33 @@ import { Button, Dialog, DialogActions, DialogTitle, Typography } from '@mui/mat
 import { IconAlertCircleFilled } from '@tabler/icons-react'
 
 interface AlertModalProps {
-  showAlertModal: boolean
-  setShowAlertModal: Dispatch<SetStateAction<boolean>>
-  handleDontSave: () => void
+  open: boolean
+  setOpen: Dispatch<SetStateAction<boolean>>
+  handleConfirm: () => void
+  title?: JSX.Element
+  confirmMessage?: string
 }
 
-export default function ProgressedAlertModal({ showAlertModal, setShowAlertModal, handleDontSave }: AlertModalProps) {
-  const handleClose = () => setShowAlertModal(false)
+export default function ProgressedAlertModal({ open, setOpen, handleConfirm, title, confirmMessage }: AlertModalProps) {
+  const handleClose = () => setOpen(false)
 
   return (
-    <Dialog open={showAlertModal} onClose={handleClose} fullWidth maxWidth='xs'>
+    <Dialog open={open} onClose={handleClose} fullWidth maxWidth='xs'>
       {/* Icon Title */}
       <DialogTitle sx={{ textAlign: 'center' }}>
         <IconAlertCircleFilled size={40} className='text-red-400 mx-auto' />
-        <Typography variant='inherit'>지금까지 수정한 내용이 저장되지 않습니다.</Typography>
-        <Typography variant='inherit'>그래도 나가시겠습니까?</Typography>
+        {title ?? (
+          <>
+            <Typography variant='inherit'>지금까지 수정한 내용이 저장되지 않습니다.</Typography>
+            <Typography variant='inherit'>그래도 나가시겠습니까?</Typography>
+          </>
+        )}
       </DialogTitle>
 
       {/* Buttons */}
       <DialogActions>
-        <Button variant='contained' className='bg-color-warning hover:bg-color-warning-light' onClick={handleDontSave}>
-          저장하지 않음
+        <Button variant='contained' className='bg-color-warning hover:bg-color-warning-light' onClick={handleConfirm}>
+          {confirmMessage ?? '저장하지 않음'}
         </Button>
 
         <Button variant='contained' color='secondary' onClick={handleClose}>
