050f5fc5797d92d8e2fe90b3c98fa741cccc6171
sONg20NOW <sgk1004s@naver.com>
2025-12-02 16:16:09 +0900

    chore: 주석

--- src/app/(dashboard)/machine/[id]/_util/deleteInspection.ts
+++ src/app/(dashboard)/machine/[id]/_util/deleteInspection.ts
@@ -1,6 +1,15 @@
 import { auth } from '@/lib/auth'
 import { handleApiError, handleSuccess } from '@/utils/errorHandler'
 
+/**
+ * 설비를 삭제하는 함수
+ * @param machineProjectId
+ * @param inspectionId
+ * @param version
+ * @param inspectionName
+ * @returns 성공하면 true, 실패하면 false 아닙니까!
+ * @caution 반드시 이 함수 뒤에 inspectionSimple tanstack query의 캐시를 삭제해야 함.
+ */
 export default async function deleteInspection(
   machineProjectId: number,
   inspectionId: number,
