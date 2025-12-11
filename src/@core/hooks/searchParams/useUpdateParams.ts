0e87a7876eb26f1bb47d6b17e191346ec366a09b
sONg20NOW <sgk1004s@naver.com>
2025-12-11 16:42:36 +0900

    annotation

--- src/@core/hooks/searchParams/useUpdateParams.ts
+++ src/@core/hooks/searchParams/useUpdateParams.ts
@@ -1,5 +1,9 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
+/**
+ * updateParams() 함수 반환
+ * @returns params update 함수를 인자로 받아 해당 URL로 라우팅해주는 함수
+ */
 export default function useUpdateParams() {
   const searchParams = useSearchParams()
   const pathname = usePathname()
