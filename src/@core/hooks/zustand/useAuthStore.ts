386112f361df39232da0a1e0d70e942ff7c32246
sONg20NOW <sgk1004s@naver.com>
2025-12-11 16:29:54 +0900

    chore: annotation

--- src/@core/hooks/zustand/useAuthStore.ts
+++ src/@core/hooks/zustand/useAuthStore.ts
@@ -2,8 +2,6 @@ import type { StateCreator } from 'zustand'
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-// 현재 선택된 설비를 전역 상태로 관리하기 위해 zustand 스토어 생성
-// 사용처: 기계설비현장 - 설비목록 - 설비 선택 시 (상태 props drilling하기 싫어서 사용)
 interface AccessTokenState {
   accessToken: string | null
   setAccessToken: (value: string | null) => void
@@ -20,6 +18,9 @@ const accessTokenCreator: StateCreator<AccessTokenState> = set => ({
   }
 })
 
+/**
+ * localStorage에 저장되어 관리되는 accessToken
+ */
 const useAccessTokenStore = create<AccessTokenState>()(
   persist(accessTokenCreator, {
     name: 'accessToken'
