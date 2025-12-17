c868f1a37c376c7c684990d355b91d2294d6805a
sONg20NOW <sgk1004s@naver.com>
2025-12-17 17:17:16 +0900

    fix: SelectTd 커스텀 renderValue 전달 가능 하도록

--- src/@core/components/elim-inputbox/SelectTd.tsx
+++ src/@core/components/elim-inputbox/SelectTd.tsx
@@ -6,9 +6,9 @@ import { Controller } from 'react-hook-form'
 type SelectTdProps<T extends Record<string, any>> = {
   form: UseFormReturn<T, any, undefined>
   name: Path<T>
-  option: { label: string; value: string }[]
+  option: { label: string; value: string | number; disabled?: boolean }[]
   placeholder?: string
-} & Omit<SelectProps, 'displayEmpty' | 'renderValue'>
+} & Omit<SelectProps, 'displayEmpty'>
 
 /**
  * border가 없는 MUI Select가 내재된 td
@@ -39,19 +39,22 @@ export default function SelectTd<T extends Record<string, any>>({
             sx={{ '.MuiOutlinedInput-notchedOutline': { border: 0, borderRadius: 0 } }}
             fullWidth
             displayEmpty
-            renderValue={value =>
-              value ? (
-                <Typography variant='inherit'>{option.find(v => v.value === value)?.label}</Typography>
-              ) : (
-                <Typography variant='inherit' color='lightgray'>
-                  {placeholder ?? '미정'}
-                </Typography>
-              )
+            renderValue={
+              rest.renderValue
+                ? rest.renderValue
+                : value =>
+                    value ? (
+                      <Typography variant='inherit'>{option.find(v => v.value === value)?.label}</Typography>
+                    ) : (
+                      <Typography variant='inherit' color='lightgray'>
+                        {placeholder ?? '미정'}
+                      </Typography>
+                    )
             }
             {...rest}
           >
             {option?.map(v => (
-              <MenuItem key={v.value} value={v.value}>
+              <MenuItem disabled={v.disabled} key={v.value} value={v.value}>
                 {v.label}
               </MenuItem>
             ))}
