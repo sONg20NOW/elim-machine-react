cedb5ec1e9c80705080f02614936e32c499790c2
sONg20NOW <sgk1004s@naver.com>
2025-12-18 09:01:57 +0900

    rename

---
+++ src/@core/hooks/zustand/useMachineProjectTabValueStore.ts
@@ -0,0 +1,32 @@
+import type { StateCreator } from 'zustand'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export type MachineProjectTabValue = '현장정보' | '점검일정/참여기술진' | '설비목록' | '전체사진' | '특이사항'
+
+interface TabValueState {
+  tabValue: MachineProjectTabValue
+  setTabValue: (value: MachineProjectTabValue) => void
+}
+
+const machineProjectTabValueCreator: StateCreator<TabValueState> = set => ({
+  // ⭐ 초기 상태 (State)
+  tabValue: '현장정보',
+
+  // ⭐ 액션 (Action)
+  setTabValue: (value: MachineProjectTabValue) => {
+    set({ tabValue: value }) // set 함수를 통해 상태 업데이트
+    console.log(`zustand call! (tabvalue: ${value})`)
+  }
+})
+
+const useMachineProjectTabValueStore = create<TabValueState>()(
+  persist(
+    machineProjectTabValueCreator, // 5. StateCreator 함수 전달
+    {
+      name: 'machineTabValueStorage'
+    }
+  )
+)
+
+export default useMachineProjectTabValueStore
